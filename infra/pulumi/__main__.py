5ae625f2a4f3252bc2cbeded4f632eab858bd2d8
Inm0rt4LKoff33 <fabimv23@gmail.com>
2026-02-20 14:16:58 -0600

    iaac inii with pulumi

---
+++ infra/pulumi/__main__.py
@@ -0,0 +1,10 @@
+import pulumi
+import pulumi_aws as aws
+
+# Create an S3 bucket
+bucket = aws.s3.Bucket("my-bucket",
+    tags={"Environment": "dev"}
+)
+
+# Export the bucket name
+pulumi.export("bucket_name", bucket.id)
